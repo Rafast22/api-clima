b7b682ed44dbec690095a2ec47141a417acc8390
Rafael Tres Sanches <71010784+Rafast22@users.noreply.github.com>
2024-10-20 18:54:14 -0300

    interceptor, login melhorado e as demais do ultimo commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { ClimaComponent } from './pages/modulos/clima/clima.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: "", component: LoginComponent, pathMatch: "full" },
+    { path: "", component: LoginComponent, pathMatch: "full",  },
     { path: "login", component: LoginComponent, pathMatch: "full" },
     { path: "register", component: RegisterComponent, pathMatch: "full" },
-    { path: "principal", component: PrincipalComponent, pathMatch: "full" },
-    { path: "clima", component: ClimaComponent, pathMatch: "full" },
+    { path: "principal", component: PrincipalComponent, pathMatch: "full", canActivate: [authGuard] },
+    
  
   ];
   
--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,72 +1,127 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { User } from './user';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  
+  tokenKey:string = 'token';
+  private headers:HttpHeaders
+  private baseUrl = 'http://192.168.0.29:8000/api/'
+  isLoggedInSubject = new BehaviorSubject<boolean>(false);
 
-  private baseUrl = 'http://192.168.0.29:8000/'
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({
+      'accept': "application/json",
+      'Content-Type': 'application/x-www-form-urlencoded'  
+    });
+   }
 
-  constructor(private http: HttpClient) { }
 
-  cadastrar(usuario: any) {
-    return this.http.post<any>(this.baseUrl, usuario)
+   getAuthenticatedHeaders(): HttpHeaders {
+    const token = this.getToken();
+    if (token) {
+      return new HttpHeaders({
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json'
+      });
+    }
+    return new HttpHeaders({
+      'Accept': 'application/json'
+    });
+  }
+  setToken(token?: string | undefined){
+    // First, serialize it (but just if token is not string type).
+    if(!token) return;
+    // const tokenString:string = JSON.stringify( token );
+    localStorage.setItem(this.tokenKey, token);
+  }
+  getToken(): string{
+    const token_string = localStorage.getItem( this.tokenKey ) ?? "";
+    let token:string = "";
+    if( token_string !=null){
+      token = token_string;
+    }
+    return token;
   }
 
-   async login(usuario: Usuario):Promise<boolean> {
-    
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded'  
+  logoutUser() {
+    localStorage.removeItem(this.tokenKey)
+  }
 
-    });
+  public async cadastrar(usuario: User) {
+    return await this.http.post<any>(`${this.baseUrl}/register`, usuario, { headers:this.headers }).toPromise().then(r => {
+      console.log(r);
+    })
+  }
+
+  public async login(usuario: User):Promise<boolean> {
     let ret:boolean=false
     const body = new HttpParams()
       .set('grant_type', 'password')
-      .set('username', usuario.username)
-      .set('password', usuario.password) 
-
+      .set('username', usuario.username ?? "")
+      .set('password', usuario.password ?? "") 
       .set('client_id', 'string') 
       .set('client_secret', 'string'); 
-      await this.http.post(`${this.baseUrl}login`, body.toString(), { headers }).toPromise().then(r =>{
-      ret=true
-    })
-    .catch(error => {
-      ret=false
-    })
+      await this.http.post<Token>(`${this.baseUrl}login`, body.toString(), { headers:this.headers }).toPromise().then(r =>{
+        if(r){
+          this.setToken(r.access_token)
+          ret = true;
+          localStorage.setItem('isLoggedIn', 'true');
+          return;
+        }
+        ret = false;
+      })
+      .catch(error => {
+        console.log(error)
+      });
     return ret
   }
-
-  getUsuarios() {
-    //return this.http.get<Array<any>>(this.apany)
-    return [];
+  
+  public async EditarUsuario(usuario: User) {
+    const body = this.getAuthenticatedHeaders()
+      .set('Content-Type', 'application/json')
+      .set('accept', 'application/json');
+    return await this.http.put<any>(`${this.baseUrl}/user`, usuario, { headers:this.headers }).toPromise().then();
   }
 
-  deletarUsuario(id: number) {
-    //return this.http.delete<any>(`${this.apany}/${id}`)
-    return true;
+  async checkAuthStatus():Promise<boolean> {
+    const body = this.getAuthenticatedHeaders()
+    .set('Content-Type', 'application/json')
+    .set('accept', 'application/json');
+    return new Promise<boolean>((resolve) => {
+      this.http.get<any>(`${this.baseUrl}status`, { headers: this.headers }).subscribe(
+        (r) => {
+          if (r['logged'] === true) {
+            resolve(true);
+          } else {
+            this.logoutUser();
+            resolve(false);
+          }
+        },
+        (error) => {
+          if (error.status = 401){
+            console.error('Error checking authentication status:', error);
+            resolve(false); 
+            return;
+          }
+          
+          
+        }
+      );
+    });
   }
 
-  editarUsuario(usuario: any) {
-    return this.http.put<any>(`${this.baseUrl}/${usuario.id}`, usuario)
-  }
 
  
 }
 
-class Usuario{
-  username:string;
-  password:string;
-  grant_type='password';
-  scope=""
-  client_id='string'
-  client_secret='string'
-  constructor(username:string, password:string){
-    this.username = username;
-    this.password = password;
-    this.grant_type = 'password'
-    this.client_id = 'string'
-    this.client_secret = 'string'
-  }
+interface Token{
+  access_token:string;
+  token_type:string;
 }
+
