693c0d57fca45e9f6301637ed0b3ac69f1499551
Rafael Sanches Tres <71010784+Rafast22@users.noreply.github.com>
2025-01-27 20:01:01 -0300

    remove cache files, change database file, add docker compose setup

--- _api/app/database.py
+++ _api/app/database.py
@@ -3,18 +3,45 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+import os
+import sys
+gettrace = getattr(sys, 'gettrace', None)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLITE_URL = "sqlite:///./sql_app.db"
+DATABASE_URL = os.getenv('DATABASE_URL')
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = os.getenv('SMTP_PORT')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+IS_DOCKER = os.getenv('IS_DOCKER')
+
+# DATABASE_URL = POSTGRESQL_URL
+# DATABASE_URL = SQLITE_URL
+# DATABASE_URL = 'postgresql://postgres:Rei12Rom%40@localhost:5432/DB'
+
+if not IS_DOCKER:
+    if gettrace():
+        SECRET_KEY = 'test'
+        ACCESS_TOKEN_EXPIRE_MINUTES = 1000
+        ALGORITHM = 'HS256'
+        engine = create_engine(DATABASE_URL)
+
+    elif DATABASE_URL == SQLITE_URL:
+        engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+        SECRET_KEY = "test"
+        ACCESS_TOKEN_EXPIRE_MINUTES = 1000
+        ALGORITHM = 'HS256'
+    else:
+        SECRET_KEY = os.getenv('SECRET_KEY')
+        engine = create_engine(DATABASE_URL)
+
+
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "8b5a1650bd34eda3385b11cc27ba36e783ec966e3cb5ca38e96bad77d417569cc33c9d2be10f470b32b277067623ccbe577f00228710bbbe3744197d4f92"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
