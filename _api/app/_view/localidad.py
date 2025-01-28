c8b4e6ac691bdd45080f18a4964a271ebb66cdb2
Rafael Sanches Tres <71010784+Rafast22@users.noreply.github.com>
2025-01-28 20:05:11 -0300

    Refactored Localidad endpoint and functions

--- _api/app/_view/localidad.py
+++ _api/app/_view/localidad.py
@@ -5,52 +5,58 @@ from sqlalchemy.orm import Session
 from ..interceptor.nasa_request import get_history_date
 from fastapi import HTTPException, status, Depends
 
-async def update(db: Session, u: RequestLocalidad) -> RequestLocalidad:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Cultivo not found",
-        headers={"WWW-Authenticate": "Bearer"},
-    )    
-   
-    db_localidad = localidad.get_by_id(db, u.id)
-    if db_localidad is None:
-        raise credentials_exception
-        
-    db_localidad = localidad.update(db, u)
+async def update_localidad(db: Session, u: RequestLocalidad) -> RequestLocalidad:
 
-    return db_localidad
-
-    # if not db_localidad:
-    #     raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    # return db_localidad
-
-async def get_by_id(db: Session, localidad_id:int ) -> RequestLocalidad:
-    db_localidad = localidad.get_by_id(db, localidad_id)
-    if not db_localidad:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Localidad not found")
-    return db_localidad
-
-async def delete(db: Session, localidad_id:int ):
     try:
+        db_localidad = localidad.get_by_id(db, u.id)
+        if not db_localidad:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        db_localidad = localidad.update(db, u)
+    except :
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return db_localidad
+
+async def get_localidad_by_id(db: Session, localidad_id:int ) -> RequestLocalidad:
+    try:
+        db_localidad = localidad.get_by_id(db, localidad_id)
+        if not db_localidad:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    except :
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return db_localidad
+
+async def delete_localidad_by_id(db: Session, localidad_id:int ):
+    try:
+        db_localidad = localidad.get_by_id(db, localidad_id)
+        if not db_localidad:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         localidad.delete(db, localidad_id)
-        return {"ok": True}
     except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-async def get_by_cultivo_id(db: Session, cultivo_id:int ) -> RequestLocalidad:
-    db_localidad = localidad.get_by_cultivo_id(db, cultivo_id)
-    if not db_localidad:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Localidad not found")
+async def get_by_localidad_by_cultivo(db: Session, cultivo_id:int ) -> RequestLocalidad:
+    try:
+        db_localidad = localidad.get_by_cultivo_id(db, cultivo_id)
+        if not db_localidad:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    except :
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return db_localidad
 
-async def get_by_user_id(db: Session, cultivo_id:int ) -> RequestLocalidad:
-    db_localidad = localidad.get_by_user_id(db, cultivo_id)
-    if not db_localidad:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Localidad not found")
+async def get_localidades_by_user_id(db: Session, cultivo_id:int ) -> RequestLocalidad:
+    try:
+        db_localidad = localidad.get_by_user_id(db, cultivo_id)
+        if not db_localidad:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    except :
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return db_localidad
 
-async def create(db: Session, u: RequestLocalidadCreate) -> RequestLocalidad:
-    history_data:list[RequestDataCreate] = await get_history_date(u.latitude, u.longitude)
-    db_localidad = localidad.create(db, u)
-    nasa_data.create_bulk(db, history_data,db_localidad.id)
+async def create_localidad(db: Session, u: RequestLocalidadCreate) -> RequestLocalidad:
+    try:
+        # history_data:list[RequestDataCreate] = await get_history_date(u.latitude, u.longitude)
+        db_localidad = localidad.create(db, u)
+        # nasa_data.create_bulk(db, history_data,db_localidad.id)
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return db_localidad
